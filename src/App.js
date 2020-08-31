a1d771e5cc71604ce875027720aed49e789890d4
nick-echevarria <nick.echev@gmail.com>
2020-08-30 23:47:06 -0400

    font applied

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import Login from './components/Login'
 import Game from './components/Game'
 import Home from './components/Home'
+import './css/index.css'
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './css/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
-import './assets/css/fonts.css'
 
 ReactDOM.render(
   <React.StrictMode>
