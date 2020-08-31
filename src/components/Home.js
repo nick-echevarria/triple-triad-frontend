fb58f60e3f4a78715ef82b0ba9f92f4fdecf1906
nick-echevarria <nick.echev@gmail.com>
2020-08-31 14:50:40 -0400

    Fleshing out login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import '../css/Home.css'
+import LoginForm from './LoginForm'
 import Box from "../images/info-box.png"
 
 const Home = () => {
     return (
         <div className="home-container">
             <div className="home-1">
-                <div className="header-1"></div>
-                <p className="title">Triple Triad</p>
-                <img className="header-2" src={Box} alt="title"/>                
+                <div className="header-1"></div>      
+                <div className="header-2">
+                    <img className="title-box" src={Box} alt="title"/>  
+                    <div className="title">Triple Triad</div>                                      
+                </div>
                 <div className="header-3"></div>
             </div>
             <div className="home-2">
                 <div className="menu-1"/>
-                <img className="menu-2" src={Box} alt="menu"/>
+                <LoginForm />
                 <div className="menu-3"/>
             </div>
             <div className="home-3"/>
-            {/* <div className="home-4"/>
-            <div className="home-5"/>
-            <div className="home-6"/>
-            <div className="home-7"/>
-            <div className="home-8"/>
-            <div className="home-9"/> */}
         </div>
     );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../css/Login.css'
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <form action='' method='POST'>
+                <ul>
+                    <li>
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="user_name"/>
+                    </li>
+                    <li>
+                        <label for="mail">E-mail:</label>
+                        <input type="email" id="mail" name="user_email"/>
+                    </li>
+                    <li>
+                        <label for="msg">Message:</label>
+                        <textarea id="msg" name="user_message"></textarea>
+                    </li>
+                </ul>                         
+            </form>
+        </div>
+    );
+}
+
+export default Login;
