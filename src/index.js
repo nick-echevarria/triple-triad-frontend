67489264d8e184a631a338347ec625828c0de215
nick-echevarria <nick.echev@gmail.com>
2020-09-30 18:53:15 -0400

    installed firebase

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './css/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
-import 'bulma/css/bulma.css' 
+import 'bulma/css/bulma.css';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCdeI7Ad_GH1PmRpBa_L4QQDbzOVH56dJI",
+  authDomain: "triple-triad-f51dc.firebaseapp.com",
+  databaseURL: "https://triple-triad-f51dc.firebaseio.com",
+  projectId: "triple-triad-f51dc",
+  storageBucket: "triple-triad-f51dc.appspot.com",
+  messagingSenderId: "1080121603949",
+  appId: "1:1080121603949:web:1b460a6e47d145df9bcc64"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
