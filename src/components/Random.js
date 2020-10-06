ed99ed789d7672fb45d3e22c1d6ffcc600d78178
nick-echevarria <nick.echev@gmail.com>
2020-10-06 18:41:26 -0400

    added board

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Board from './Board'
 
 const Random = () => {
     return (
         <div>
-            Hi!
+            <Board></Board>
         </div>
     );
 }
