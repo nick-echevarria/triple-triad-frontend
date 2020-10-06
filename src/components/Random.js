1d333cde02bbc4de709d8d28e30ff6fe002971c9
nick-echevarria <nick.echev@gmail.com>
2020-10-06 18:44:21 -0400

    sign up

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Board from './Board'
+import SignUp from './SignUp'
 
 const Random = () => {
     return (
         <div>
             <Board></Board>
+            <SignUp></SignUp>
         </div>
     );
 }
